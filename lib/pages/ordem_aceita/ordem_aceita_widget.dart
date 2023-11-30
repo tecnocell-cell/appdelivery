6ccedd0a947d4918e5bfd648e35da7de87bbfee1
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-11-30 02:13:39 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/ordem_aceita/ordem_aceita_widget.dart
+++ lib/pages/ordem_aceita/ordem_aceita_widget.dart
@@ -55,138 +55,129 @@ class _OrdemAceitaWidgetState extends State<OrdemAceitaWidget> {
       child: Scaffold(
         key: scaffoldKey,
         backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
-        body: SafeArea(
-          top: true,
-          child: Container(
-            width: double.infinity,
-            height: double.infinity,
-            decoration: BoxDecoration(
-              color: FlutterFlowTheme.of(context).secondaryBackground,
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: Image.asset(
-                  'assets/images/Mask_Group_(1).png',
-                ).image,
-              ),
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+            color: FlutterFlowTheme.of(context).secondaryBackground,
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: Image.asset(
+                'assets/images/Mask_Group_(1).png',
+              ).image,
             ),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Padding(
-                  padding: const EdgeInsetsDirectional.fromSTEB(0.0, 120.0, 0.0, 0.0),
-                  child: Lottie.asset(
-                    'assets/lottie_animations/bpAVO8ShV9.json',
-                    width: 310.0,
-                    height: 310.0,
-                    fit: BoxFit.cover,
-                    reverse: true,
-                    animate: true,
-                  ),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsetsDirectional.fromSTEB(0.0, 120.0, 0.0, 0.0),
+                child: Lottie.asset(
+                  'assets/lottie_animations/bpAVO8ShV9.json',
+                  width: 310.0,
+                  height: 310.0,
+                  fit: BoxFit.cover,
+                  reverse: true,
+                  animate: true,
                 ),
-                Flexible(
-                  child: Container(
-                    width: 350.0,
-                    height: 150.0,
-                    decoration: const BoxDecoration(),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        Text(
-                          'Parabéns',
-                          textAlign: TextAlign.center,
-                          style:
-                              FlutterFlowTheme.of(context).bodyMedium.override(
-                                    fontFamily: 'Readex Pro',
-                                    fontSize: 30.0,
-                                    letterSpacing: 1.0,
-                                    fontWeight: FontWeight.w500,
-                                  ),
-                        ),
-                        Text(
-                          'Pagamento Confirmado',
-                          textAlign: TextAlign.center,
-                          style:
-                              FlutterFlowTheme.of(context).bodyMedium.override(
-                                    fontFamily: 'Readex Pro',
-                                    fontSize: 28.0,
-                                    fontWeight: FontWeight.w500,
-                                  ),
-                        ),
-                        Text(
-                          'Pedido Enviado para Preparo! ',
-                          textAlign: TextAlign.center,
-                          style: FlutterFlowTheme.of(context)
-                              .bodyMedium
-                              .override(
-                                fontFamily: 'Readex Pro',
-                                color:
-                                    FlutterFlowTheme.of(context).secondaryText,
-                                fontSize: 16.0,
-                                fontWeight: FontWeight.w300,
-                              ),
-                        ),
-                      ].divide(const SizedBox(height: 12.0)),
-                    ),
-                  ),
-                ),
-                Container(
-                  width: 300.0,
+              ),
+              Flexible(
+                child: Container(
+                  width: 350.0,
                   height: 150.0,
                   decoration: const BoxDecoration(),
                   child: Column(
                     mainAxisSize: MainAxisSize.max,
                     children: [
-                      FFButtonWidget(
-                        onPressed: () {
-                          print('Button pressed ...');
-                        },
-                        text: 'Acompanhar Pedido',
-                        options: FFButtonOptions(
-                          width: 364.0,
-                          height: 67.0,
-                          padding: const EdgeInsetsDirectional.fromSTEB(
-                              24.0, 0.0, 24.0, 0.0),
-                          iconPadding: const EdgeInsetsDirectional.fromSTEB(
-                              0.0, 0.0, 0.0, 0.0),
-                          color: FlutterFlowTheme.of(context).primary,
-                          textStyle:
-                              FlutterFlowTheme.of(context).titleSmall.override(
-                                    fontFamily: 'Readex Pro',
-                                    color: Colors.white,
-                                    fontSize: 20.0,
-                                  ),
-                          elevation: 3.0,
-                          borderSide: const BorderSide(
-                            color: Colors.transparent,
-                            width: 1.0,
-                          ),
-                          borderRadius: BorderRadius.circular(19.0),
-                        ),
+                      Text(
+                        'Parabéns',
+                        textAlign: TextAlign.center,
+                        style: FlutterFlowTheme.of(context).bodyMedium.override(
+                              fontFamily: 'Readex Pro',
+                              fontSize: 30.0,
+                              letterSpacing: 1.0,
+                              fontWeight: FontWeight.w500,
+                            ),
                       ),
-                      InkWell(
-                        splashColor: Colors.transparent,
-                        focusColor: Colors.transparent,
-                        hoverColor: Colors.transparent,
-                        highlightColor: Colors.transparent,
-                        onTap: () async {
-                          context.pushNamed('delivery');
-                        },
-                        child: Text(
-                          'Voltar a Restaurante',
-                          style:
-                              FlutterFlowTheme.of(context).bodyMedium.override(
-                                    fontFamily: 'Readex Pro',
-                                    fontSize: 18.0,
-                                    fontWeight: FontWeight.w500,
-                                  ),
-                        ),
+                      Text(
+                        'Pagamento Confirmado',
+                        textAlign: TextAlign.center,
+                        style: FlutterFlowTheme.of(context).bodyMedium.override(
+                              fontFamily: 'Readex Pro',
+                              fontSize: 28.0,
+                              fontWeight: FontWeight.w500,
+                            ),
+                      ),
+                      Text(
+                        'Pedido Enviado para Preparo! ',
+                        textAlign: TextAlign.center,
+                        style: FlutterFlowTheme.of(context).bodyMedium.override(
+                              fontFamily: 'Readex Pro',
+                              color: FlutterFlowTheme.of(context).secondaryText,
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.w300,
+                            ),
                       ),
                     ].divide(const SizedBox(height: 12.0)),
                   ),
                 ),
-              ],
-            ),
+              ),
+              Container(
+                width: 300.0,
+                height: 150.0,
+                decoration: const BoxDecoration(),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    FFButtonWidget(
+                      onPressed: () {
+                        print('Button pressed ...');
+                      },
+                      text: 'Acompanhar Pedido',
+                      options: FFButtonOptions(
+                        width: 364.0,
+                        height: 67.0,
+                        padding: const EdgeInsetsDirectional.fromSTEB(
+                            24.0, 0.0, 24.0, 0.0),
+                        iconPadding:
+                            const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
+                        color: FlutterFlowTheme.of(context).primary,
+                        textStyle:
+                            FlutterFlowTheme.of(context).titleSmall.override(
+                                  fontFamily: 'Readex Pro',
+                                  color: Colors.white,
+                                  fontSize: 20.0,
+                                ),
+                        elevation: 3.0,
+                        borderSide: const BorderSide(
+                          color: Colors.transparent,
+                          width: 1.0,
+                        ),
+                        borderRadius: BorderRadius.circular(19.0),
+                      ),
+                    ),
+                    InkWell(
+                      splashColor: Colors.transparent,
+                      focusColor: Colors.transparent,
+                      hoverColor: Colors.transparent,
+                      highlightColor: Colors.transparent,
+                      onTap: () async {
+                        context.pushNamed('delivery');
+                      },
+                      child: Text(
+                        'Voltar a Restaurante',
+                        style: FlutterFlowTheme.of(context).bodyMedium.override(
+                              fontFamily: 'Readex Pro',
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.w500,
+                            ),
+                      ),
+                    ),
+                  ].divide(const SizedBox(height: 12.0)),
+                ),
+              ),
+            ],
           ),
         ),
       ),
